2ea2f318330e220f71c0f1f5d68bf0a0794363f3
lenovog7803-lgtm <lenovog7803@gmail.com>
2026-07-16 13:08:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш
-const CACHE = 'kopilka-v21';
+const CACHE = 'kopilka-v32';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
