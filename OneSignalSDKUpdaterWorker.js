69f45d13dc8804040d9c9876f0233cff9dcd45be
lenovog7803-lgtm <lenovog7803@gmail.com>
2026-07-23 16:24:33 +0300

    Add files via upload

---
+++ OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,3 @@
+// Вспомогательный файл, который OneSignal использует для проверки обновлений SDK.
+// Должен существовать рядом с основным воркером.
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
